77058af6f574983ab86b729f5c0c3164a59f0844
Arcosdarniel <danielorlando772@gmail.com>
2025-03-11 18:53:22 -0300

    add class, jobs and resume

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job Job1 = new Job();
+        Job1._jobTitle = "Software Engineer";
+        Job1._company = "Microsoft";
+        Job1._startYear = 2025;
+        Job1._endYear = 2026;
+
+        Job Job2 = new Job();
+        Job2._jobTitle = "Manager";
+        Job2._company = "Apple";
+        Job2._startYear = 2025;
+        Job2._endYear =2026;
+
+        Resume Resume1 = new Resume();
+        Resume1._name = "Marcos Arcos";
+
+        Resume1.jobs.Add(Job1);
+        Resume1.jobs.Add(Job2);
+
+        Resume1.Display();
     }
 }
