cb978003723464c243411bab1773a1fb7d156947
Arcosdarniel <danielorlando772@gmail.com>
2025-04-07 21:09:37 -0400

    areaHomework

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(new Circle("Red", 5));
+        shapes.Add(new Rectangle("Blue", 5, 5));
+        shapes.Add(new Square("Green", 5));
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine(shape.GetColor());
+            Console.WriteLine(shape.GetArea());
+        }
     }
 }
